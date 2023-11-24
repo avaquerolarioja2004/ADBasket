e8930629739dbd0c36b912fb5678d228027f9f0a
Ángel  Vaquero <angelvaquerolarioja@gmail.com>
2023-11-24 11:26:34 +0100

    first commit

---
+++ src/main/java/app/back/POJOS/Jornada.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.back.POJOS;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author mrpox
+ */
+public class Jornada implements Serializable{
+    
+}
