492c0bc5c1e06a3381aa50c060467a9108cfd0fd
Ángel  Vaquero <angelvaquerolarioja@gmail.com>
2023-11-28 19:57:07 +0100

    @Entity for Equipo and Jornada

--- src/main/java/app/back/POJOS/Equipo.java
+++ src/main/java/app/back/POJOS/Equipo.java
@@ -4,10 +4,187 @@
  */
 package app.back.POJOS;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.Check;
+
 /**
  *
  * @author mrpox
  */
+
+
+@Entity
+@Table(name = "EQUIPO")
+@Check(constraints = "jugadosCasa = ganadosCasa + perdidosCasa and jugadosFuera = ganadosFuera + perdidosFuera")
 public class Equipo {
-    
+
+    @Id
+    @Column(name = "idequipo")
+    private int idEquipo;
+
+    @Column(name = "nombre", length = 25)
+    private String nombre;
+
+    @Column(name = "ciudad", length = 25)
+    private String ciudad;
+
+    @Column(name = "jugadosCasa")
+    private int jugadosCasa;
+
+    @Column(name = "ganadosCasa")
+    private int ganadosCasa;
+
+    @Column(name = "perdidosCasa")
+    private int perdidosCasa;
+
+    @Column(name = "puntosFavorCasa")
+    private int puntosFavorCasa;
+
+    @Column(name = "puntosContraCasa")
+    private int puntosContraCasa;
+
+    @Column(name = "jugadosFuera")
+    private int jugadosFuera;
+
+    @Column(name = "ganadosFuera")
+    private int ganadosFuera;
+
+    @Column(name = "perdidosFuera")
+    private int perdidosFuera;
+
+    @Column(name = "puntosFavorFuera")
+    private int puntosFavorFuera;
+
+    @Column(name = "puntosContraFuera")
+    private int puntosContraFuera;
+
+    // Constructor, getters y setters
+
+    // Constructor por defecto
+    public Equipo() {
+    }
+
+    // Constructor con parámetros
+    public Equipo(int idEquipo, String nombre, String ciudad, int jugadosCasa, int ganadosCasa, int perdidosCasa,
+                  int puntosFavorCasa, int puntosContraCasa, int jugadosFuera, int ganadosFuera, int perdidosFuera,
+                  int puntosFavorFuera, int puntosContraFuera) {
+        this.idEquipo = idEquipo;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.jugadosCasa = jugadosCasa;
+        this.ganadosCasa = ganadosCasa;
+        this.perdidosCasa = perdidosCasa;
+        this.puntosFavorCasa = puntosFavorCasa;
+        this.puntosContraCasa = puntosContraCasa;
+        this.jugadosFuera = jugadosFuera;
+        this.ganadosFuera = ganadosFuera;
+        this.perdidosFuera = perdidosFuera;
+        this.puntosFavorFuera = puntosFavorFuera;
+        this.puntosContraFuera = puntosContraFuera;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public int getJugadosCasa() {
+        return jugadosCasa;
+    }
+
+    public int getGanadosCasa() {
+        return ganadosCasa;
+    }
+
+    public int getPerdidosCasa() {
+        return perdidosCasa;
+    }
+
+    public int getPuntosFavorCasa() {
+        return puntosFavorCasa;
+    }
+
+    public int getPuntosContraCasa() {
+        return puntosContraCasa;
+    }
+
+    public int getJugadosFuera() {
+        return jugadosFuera;
+    }
+
+    public int getGanadosFuera() {
+        return ganadosFuera;
+    }
+
+    public int getPerdidosFuera() {
+        return perdidosFuera;
+    }
+
+    public int getPuntosFavorFuera() {
+        return puntosFavorFuera;
+    }
+
+    public int getPuntosContraFuera() {
+        return puntosContraFuera;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setJugadosCasa(int jugadosCasa) {
+        this.jugadosCasa = jugadosCasa;
+    }
+
+    public void setGanadosCasa(int ganadosCasa) {
+        this.ganadosCasa = ganadosCasa;
+    }
+
+    public void setPerdidosCasa(int perdidosCasa) {
+        this.perdidosCasa = perdidosCasa;
+    }
+
+    public void setPuntosFavorCasa(int puntosFavorCasa) {
+        this.puntosFavorCasa = puntosFavorCasa;
+    }
+
+    public void setPuntosContraCasa(int puntosContraCasa) {
+        this.puntosContraCasa = puntosContraCasa;
+    }
+
+    public void setJugadosFuera(int jugadosFuera) {
+        this.jugadosFuera = jugadosFuera;
+    }
+
+    public void setGanadosFuera(int ganadosFuera) {
+        this.ganadosFuera = ganadosFuera;
+    }
+
+    public void setPerdidosFuera(int perdidosFuera) {
+        this.perdidosFuera = perdidosFuera;
+    }
+
+    public void setPuntosFavorFuera(int puntosFavorFuera) {
+        this.puntosFavorFuera = puntosFavorFuera;
+    }
+
+    public void setPuntosContraFuera(int puntosContraFuera) {
+        this.puntosContraFuera = puntosContraFuera;
+    }
 }
+
--- src/main/java/app/back/POJOS/Jornada.java
+++ src/main/java/app/back/POJOS/Jornada.java
@@ -4,12 +4,51 @@
  */
 package app.back.POJOS;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
+import java.sql.Date;
 
 /**
  *
  * @author mrpox
  */
-public class Jornada implements Serializable{
-    
+@Entity
+@Table(name = "JORNADA")
+public class Jornada {
+
+    @Id
+    @Column(name = "numjornada")
+    private int numJornada;
+
+    @Column(name = "fecha")
+    private Date fecha;
+
+    // Constructor, getters y setters
+
+    // Constructor por defecto
+    public Jornada() {
+    }
+
+    // Constructor con parámetros
+    public Jornada(int numJornada, Date fecha) {
+        this.numJornada = numJornada;
+        this.fecha = fecha;
+    }
+
+    // Getters y Setters
+
+    public int getNumJornada() {
+        return numJornada;
+    }
+
+    public void setNumJornada(int numJornada) {
+        this.numJornada = numJornada;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
